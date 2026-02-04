e47bfb5356c81d0dc412b21be3e7e51d1a6d097c
Shreyansh Srivastava <shreyansh.official.6726@gmail.com>
2026-02-04 20:30:47 +0530

    bug fix

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API = axios.create({
   // Accesses the Render URL from your .env file
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api', 
+  baseURL: process.env.REACT_APP_API_URL, 
 });
 
 // Interceptor to attach JWT token to every outgoing request

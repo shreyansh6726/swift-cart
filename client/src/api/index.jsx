4eadc5abe2ee1df3b5074ade70fc933938a21556
Shreyansh Srivastava <shreyansh.official.6726@gmail.com>
2026-02-09 17:24:05 +0530

    work

--- client/src/api/index.jsx
+++ client/src/api/index.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000/api',
+  baseURL: 'http://localhost:5000/api', // Hardcoded to force local connection
+  // baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000/api',
 });
 
 API.interceptors.request.use((req) => {
